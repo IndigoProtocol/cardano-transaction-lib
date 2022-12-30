3cf4f7e963b7b9bb314a63ff0689e489af86a10e
Dzmitry Shuysky <errfrom@yandex.ru>
2022-12-30 14:41:48 +0100

    Remove obsolete InsufficientTxInputs error, Update doc/faq.md

--- src/Internal/BalanceTx/BalanceTx.purs
+++ src/Internal/BalanceTx/BalanceTx.purs
@@ -38,7 +38,6 @@ import Ctl.Internal.BalanceTx.Error
       , CouldNotGetCollateral
       , CouldNotGetUtxos
       , ExUnitsEvaluationFailed
-      , InsufficientTxInputs
       , ReindexRedeemersError
       , UtxoLookupFailedFor
       , UtxoMinAdaValueCalculationFailed
